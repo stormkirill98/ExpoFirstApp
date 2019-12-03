488ce107052bb50708a269a2906486aedd2c2926
kPanihin <k.panihin@akolchin.com>
2019-12-03 14:39:33 +0300

    clear home screen

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -20,7 +20,7 @@ const HomeStack = createStackNavigator(
 );
 
 HomeStack.navigationOptions = {
-    tabBarLabel: 'Home',
+    tabBarLabel: 'Write',
     tabBarIcon: ({focused}) => (
         <TabBarIcon
             focused={focused}
@@ -43,7 +43,7 @@ const LinksStack = createStackNavigator(
 );
 
 LinksStack.navigationOptions = {
-    tabBarLabel: 'Links',
+    tabBarLabel: 'Read',
     tabBarIcon: ({focused}) => (
         <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}/>
     ),
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,5 @@
-import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
-import {Image, Platform, ScrollView, StyleSheet, Text, TouchableOpacity, View,} from 'react-native';
+import {Image, Platform, ScrollView, StyleSheet, Text, View,} from 'react-native';
 
 import {MonoText} from '../components/StyledText';
 
@@ -22,41 +21,9 @@ export default function HomeScreen() {
                 </View>
 
                 <View style={styles.getStartedContainer}>
-                    <DevelopmentModeNotice/>
 
-                    <Text style={styles.getStartedText}>Get started by opening</Text>
-
-                    <View
-                        style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-                        <MonoText>screens/HomeScreen.js</MonoText>
-                    </View>
-
-                    <Text style={styles.getStartedText}>
-                        Change this text and your app will automatically reload.
-                    </Text>
-                </View>
-
-                <View style={styles.helpContainer}>
-                    <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-                        <Text style={styles.helpLinkText}>
-                            Help, it didn’t automatically reload!
-                        </Text>
-                    </TouchableOpacity>
                 </View>
             </ScrollView>
-
-            <View style={styles.tabBarInfoContainer}>
-                <Text style={styles.tabBarInfoText}>
-                    This is a tab bar. You can edit it in:
-                </Text>
-
-                <View
-                    style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-                    <MonoText style={styles.codeHighlightText}>
-                        navigation/MainTabNavigator.js
-                    </MonoText>
-                </View>
-            </View>
         </View>
     );
 }
@@ -64,41 +31,6 @@ export default function HomeScreen() {
 HomeScreen.navigationOptions = {
     header: null,
 };
-
-function DevelopmentModeNotice() {
-    if (__DEV__) {
-        const learnMoreButton = (
-            <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-                Learn more
-            </Text>
-        );
-
-        return (
-            <Text style={styles.developmentModeText}>
-                Development mode is enabled: your app will be slower but you can use
-                useful development tools. {learnMoreButton}
-            </Text>
-        );
-    } else {
-        return (
-            <Text style={styles.developmentModeText}>
-                You are not in development mode: your app will run at full speed.
-            </Text>
-        );
-    }
-}
-
-function handleLearnMorePress() {
-    WebBrowser.openBrowserAsync(
-        'https://docs.expo.io/versions/latest/workflow/development-mode/'
-    );
-}
-
-function handleHelpPress() {
-    WebBrowser.openBrowserAsync(
-        'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
-    );
-}
 
 const styles = StyleSheet.create({
     container: {
