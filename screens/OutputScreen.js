1f34098fbab720d4dd3e3012ee3a5016e6534c53
stormkirill98 <stormkirill98@gmail.com>
2019-12-05 00:03:43 +0300

    output static list

--- screens/OutputScreen.js
+++ screens/OutputScreen.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import {ScrollView, StyleSheet} from 'react-native';
+import { ListItem } from 'react-native-elements'
+
 
 export default function OutputScreen() {
+    const list = [
+        {
+            first_name: "Oleg",
+            last_name: "Sokolov",
+            age: "25"
+        },
+        {
+            first_name: "Kirill",
+            last_name: "SAdas",
+            age: "12"
+        },
+    ];
+
     return (
         <ScrollView style={styles.container}>
+            {
+                list.map((l, i) => (
+                    <ListItem
+                        key={i}
+                        title={l.first_name + " " + l.last_name}
+                        subtitle={"Age: " + l.age}
+                        bottomDivider
+                    />
+                ))
+            }
         </ScrollView>
     );
 }
